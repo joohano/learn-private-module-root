5266864c153366ec323d2b025b3a5666f8bd027d
Johan Linder-Finstad <joohano@gmail.com>
2020-12-29 11:22:08 +0100

    changed module name

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/joohano/s3-webapp/aws"
+  source  = "app.terraform.io/joohano/s3-webapppp/aws"
   name        = var.name
   region = var.region
   prefix = var.prefix
